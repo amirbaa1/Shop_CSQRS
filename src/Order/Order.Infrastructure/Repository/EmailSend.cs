1e22e65c6ea464a78338a26d7b1cb18ecbb5f425
AMIR_bA <53426863+amirbaa1@users.noreply.github.com>
2024-07-02 17:28:06 +0330

    update

--- src/Order/Order.Infrastructure/Repository/EmailSend.cs
+++ src/Order/Order.Infrastructure/Repository/EmailSend.cs
@@ -5,20 +5,20 @@ using Order.Domain.Model.Email;
 using Order.Domain.Repository;
 
 namespace Order.Infrastructure.Repository;
-
-public class EmailSetting
-{
-    //stmp email
-    public string HOST { get; set; } = "smtp.elasticemail.com";
-    public int PORT { get; set; } = 2525;
-    public string User { get; set; } = "amir.2002.ba@gmail.com";
-    public string Password { get; set; } = "69985A8E31CB9CF00B0284B8A37C1EDEE8D8";
+//
+// public class EmailSetting
+// {
+//     //stmp email
+//     public string HOST { get; set; } = "smtp.elasticemail.com";
+//     public int PORT { get; set; } = 2525;
+//     public string User { get; set; } = "amir.2002.ba@gmail.com";
+//     public string Password { get; set; } = "69985A8E31CB9CF00B0284B8A37C1EDEE8D8";
 
     // sandGrid
     // public string ApiKey { get; set; } = "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";
     // public string FromAddress { get; set; } = "amir.2002.ba@gmail.com";
     // public string FromName { get; set; } = "shooping";
-}
+// }
 
 public class EmailSend : IEmailSend
 {
