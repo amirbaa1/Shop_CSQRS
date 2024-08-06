df1355787ffca496ea405f04325dd6a512760a79
AMIR_bA <53426863+amirbaa1@users.noreply.github.com>
2024-08-06 11:38:36 +0330

    update notification

---
+++ src/Notification/Notification.worker/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+using Common.Infrastructure.Extensions;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.RegisterVariables(builder.Configuration);
+builder.Services.RegisterMassTransit(Assembly.GetExecutingAssembly());
+builder.Services.RegisterAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.RegisterMediatR(Assembly.GetExecutingAssembly());
+
+
+var app = builder.Build();
+app.Run();
